82af1437f7397942b467fc0c4bbe23b3901d450c
Ann Song <gmsong1978@gmail.com>
2023-04-16 20:20:37 -0400

    Red coords don't work

--- PathVisualizer/main.py
+++ PathVisualizer/main.py
@@ -1,7 +1,7 @@
 import pygame
 import constants
 
-is_blue_alliance = True
+is_blue_alliance = False
 
 WINDOW_WIDTH = int(constants.FIELD_WIDTH_METERS * constants.SCALE_FACTOR)
 WINDOW_HEIGHT = int(constants.FIELD_HEIGHT_METERS * constants.SCALE_FACTOR)
@@ -19,35 +19,73 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 window.blit(scaled_field_image, (0, 0))
 
 
-def scale_to_meters(mouse_pos_pixels):
+def scale_to_meters(x, y):
+    """
+    Scales the x and y coordinates from pixels to meters
+    :param x: x position in pixels
+    :param y: y position in pixels
+    :return: x and y position in meters
+    """
+    scale = WINDOW_HEIGHT / constants.FIELD_HEIGHT_METERS
+
+    # WORKING FOR BLUE
     if is_blue_alliance:
-        scale = WINDOW_HEIGHT / constants.FIELD_HEIGHT_METERS
         x_offset = (WINDOW_WIDTH - scale * constants.FIELD_WIDTH_METERS) / 2
         y_offset = 0
-        new_x = (mouse_pos_pixels[0] - x_offset) / scale
-        new_y = (WINDOW_HEIGHT - mouse_pos_pixels[1] - y_offset) / scale
+        new_x = ((x - x_offset) / scale)
+        new_y = ((WINDOW_HEIGHT - y - y_offset) / scale)
         return new_x, new_y
+    else:  # lol idk
+        x_offset = (WINDOW_WIDTH - scale * constants.FIELD_WIDTH_METERS) / 2 + WINDOW_WIDTH
+        y_offset = WINDOW_HEIGHT
+        new_x = ((x - x_offset) / scale)
+        new_y = ((WINDOW_HEIGHT - y - y_offset) / scale)
+        return -new_x, -new_y
 
 
-def scale_to_pixels(x, y):
+def scale_to_pixels(x: float, y: float):
+    """
+    Scales the x and y coordinates from meters to pixels
+    :param x: x position in meters
+    :param y: y position in meters
+    :return: x and y position in pixels
+    """
+    scale = WINDOW_HEIGHT / constants.FIELD_HEIGHT_METERS
+
+    #  WORKING FOR BLUE
     if is_blue_alliance:
-        scale = WINDOW_HEIGHT / constants.FIELD_HEIGHT_METERS
         x_offset = (WINDOW_WIDTH - scale * constants.FIELD_WIDTH_METERS) / 2
         y_offset = 0
         new_x = int(x * scale + x_offset)
         new_y = int(WINDOW_HEIGHT - (y * scale + y_offset))
         return new_x, new_y
+    else:  # je ne sais pas
+        x_offset = (WINDOW_WIDTH - scale * constants.FIELD_WIDTH_METERS) / 2 + WINDOW_WIDTH
+        y_offset = WINDOW_HEIGHT
+        new_x = int(x * scale - x_offset)
+        new_y = int(WINDOW_HEIGHT - y + y_offset) * scale
+        return -new_x, new_y
 
 
-def draw_point(x, y):
-    pygame.draw.circle(window, (255, 0, 0), (int(x), int(y)), 10)
+def draw_point(x: float, y: float):
+    """
+    Draws a point on the field
+    :param x: x position in meters
+    :param y: y position in meters
+    """
+    pygame.draw.circle(window, (255, 0, 0), scale_to_pixels(x, y), 10)
 
 
-# BLUE CABLE GUARD 3 PIECE COORDS
-initial_x = 1.55
-initial_y = 0.51
-draw_point(*scale_to_pixels(initial_x, initial_y))
-draw_point(*scale_to_pixels(initial_x + 5.25, initial_y + 0.43))
+if is_blue_alliance:
+    initial_x = 1.55
+    initial_y = 0.51
+    draw_point(initial_x, initial_y)
+    draw_point(initial_x + 5.25, initial_y + 0.43)
+else:
+    initial_x = 1.55
+    initial_y = 7.495
+    draw_point(initial_x, initial_y)
+    draw_point(initial_x + 5.25, initial_y - 0.43)
 
 pygame.display.update()
 
@@ -56,5 +94,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        print((pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]))
 
 pygame.quit()
